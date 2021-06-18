69a9f3f602162bbaef3d67fed28a821c393cc363
DillonHeinen <56735131+DillonHeinen@users.noreply.github.com>
2021-06-18 09:43:41 -0500

    Initial commit

---
+++ src/main/java/com/example/HRCaseTracker/HrCaseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.HRCaseTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrCaseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrCaseTrackerApplication.class, args);
+	}
+
+}
